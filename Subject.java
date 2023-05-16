fe1e54a6b7537f8273fbaf6e3a332ea2d7de9984
kleber2603 <50875942+kleber2603@users.noreply.github.com>
2023-05-16 11:58:08 -0300

    Delete Subject.java

--- Subject.java
+++
@@ -1,8 +0,0 @@
-package observer.praticate.engIII;
-
-public interface Subject 
-{
- public void registerObserver(Observer o);
- public void removeObserver(Observer o);
- public void notifyObservers(String nome, String sobrenome, double salario, double novosalario);
-}

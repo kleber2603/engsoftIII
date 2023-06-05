8283ace146a0554b82388112de34ec46088e08bd
kleber2603 <50875942+kleber2603@users.noreply.github.com>
2023-06-05 10:07:04 -0300

    Delete Quadrado.java

--- Quadrado.java
+++
@@ -1,9 +0,0 @@
-package dev.jornada.design.composite;
-
-public class Quadrado implements Forma 
-{
- public void desenho(String fillColor) 
- {
-  System.out.println("Desenhando o quadrado com a cor "+fillColor);
- }
-}

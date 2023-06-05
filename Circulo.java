456cd26311d28676ba569dcafa219371ed320fe6
kleber2603 <50875942+kleber2603@users.noreply.github.com>
2023-06-05 10:09:27 -0300

    Delete Circulo.java

--- Circulo.java
+++
@@ -1,9 +0,0 @@
-﻿package dev.jornada.design.composite;
-
-public class Circulo implements Forma 
-{
- public void desenho(String fillColor) 
- {
-  System.out.println("Desenhando o círculo com a cor "+fillColor);
- }
-}

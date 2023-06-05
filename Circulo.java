5b7bd6e1e26e76ff04e6bbcad7f98909d804812b
kleber2603 <50875942+kleber2603@users.noreply.github.com>
2023-06-05 10:03:16 -0300

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,9 @@
+﻿package dev.jornada.design.composite;
+
+public class Circulo implements Forma 
+{
+ public void desenho(String fillColor) 
+ {
+  System.out.println("Desenhando o círculo com a cor "+fillColor);
+ }
+}
---
+++ Triangulo.java
@@ -0,0 +1,9 @@
+﻿package dev.jornada.design.composite;
+
+public class Triangulo implements Forma 
+{ 
+ public void desenho(String fillColor) 
+ {
+  System.out.println("Desenhando o triângulo com a cor "+fillColor);
+ }
+}

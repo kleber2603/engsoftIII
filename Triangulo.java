dae027a7d90fedfddd415e6289d1bc53d40d87a9
kleber2603 <50875942+kleber2603@users.noreply.github.com>
2023-06-05 10:07:23 -0300

    Delete Triangulo.java

--- Triangulo.java
+++
@@ -1,9 +0,0 @@
-﻿package dev.jornada.design.composite;
-
-public class Triangulo implements Forma 
-{ 
- public void desenho(String fillColor) 
- {
-  System.out.println("Desenhando o triângulo com a cor "+fillColor);
- }
-}
